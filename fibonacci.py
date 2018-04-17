dcc27d6ddbea0392dbb489c38d68c4986e4e8534
Danilo Marques <danilomarques20@hotmail.com>
2018-04-17 15:08:58 -0300

    Sequencia de fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+#função recursiva para a sequencia de fibonacci
+def fib(n):
+	#caso n seja menor que 1
+	if n < 1:
+		return 0
+	elif n == 1:
+		return 1
+	else:
+		#usando a recursividade para achar o valor correspondente a posição desejada
+		return fib(n-1) + fib(n-2)
+
+#input do usuario
+pos = int(input("Qual posição da sequencia de Fibonacci deseja encontrar? "))
+print(fib(pos))				
