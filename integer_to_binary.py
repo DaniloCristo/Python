ef3d05cc27083345ddea656a4acd882b7d27ae81
Danilo Marques <danilomarques20@hotmail.com>
2018-04-29 11:39:15 -0300

    Alterando para tratar de inteiros negativos

--- integer_to_binary.py
+++ integer_to_binary.py
@@ -1,11 +1,27 @@
 def binary(x):
-	b = []	
+	if x < 0:
+		#tratar caso o x seja negativo	
+		is_neg = True
+		x = abs(x)
+	else:
+		is_neg = False
+
+	result = ''	
+		
+	if x == 0:	
+		result = '0'
+	
 	while x > 0:
-		b.append(x % 2)
-		x = x // 2
-	print(list(reversed(b)))	
+		#acumulando result sempre adicionando o resultado a esquerda
+		result = str(x % 2) + result
+		x = x // 2	
+	if is_neg:
+		#se o x for negativo atribuir o sinal negativo a result
+		result = '-' + result
+	return result		
 
+#user input
 x = int(input("Qual numero deseja converter para binario? "))
-binary(x)
-
+r = binary(x)
+print(r)
 		
