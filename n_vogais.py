a01086eeba6823dac2a585eb6b32235d33f02e34
Danilo Marques <danilomarques20@hotmail.com>
2018-05-20 18:15:25 -0300

    Criando um array com as vogais

--- n_vogais.py
+++ n_vogais.py
@@ -1,10 +1,11 @@
 def numero_vogais(s):
 	#numero de vogais
 	nv = 0
+	vogal = ["a","e","i","o","u","A","E","I","O","U"]
 	for letter in s:
 		#verficiar cada letra da string possa ser uma vogal
-	    if letter == "A" or letter == 'a'  or letter == 'E'  or letter == 'e'  or letter == 'I'  or letter == 'i'  or letter == 'O'  or letter == 'o'  or letter == 'U'  or letter == 'u':
-	        nv += 1
+		if letter in vogal:
+			nv += 1  
 	    
 	return "O numero de vogais na string é: {0}".format(nv)
 
