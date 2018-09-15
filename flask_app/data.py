649dc01fc947aa6faae77fe795222ce77075e6c4
Danilo Marques <danilomarques20@hotmail.com>
2018-09-15 18:30:33 -0300

    Criando um app com flask

---
+++ flask_app/data.py
@@ -0,0 +1,26 @@
+def Articles():
+	articles = [
+		{
+			"id": 1,
+			"title":"Article one",
+			"body": "conteudo do artigo",
+			"author":"autor do artigo",
+			"create_date":"27-06-1999"
+		},
+		{
+			"id": 2,
+			"title":"Article Two",
+			"body": "conteudo do artigo",
+			"author":"autor do artigo",
+			"create_date":"27-06-1999"
+		},
+		{
+			"id": 3,
+			"title":"Article Three",
+			"body": "conteudo do artigo",
+			"author":"autor do artigo",
+			"create_date":"27-06-1999"
+		}
+	]
+	return articles
+	
